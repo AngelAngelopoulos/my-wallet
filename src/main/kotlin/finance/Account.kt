b585b879d29faa2af09e57787f285deadc7c5d6e
David PEREZ <David.PEREZ@ingenico.com>
2021-06-24 22:38:15 -0500

    Se agregaron constructores sobrecargados en clase Account y se probaron en main.

--- src/main/kotlin/finance/Account.kt
+++ src/main/kotlin/finance/Account.kt
@@ -3,7 +3,25 @@ package finance
 class Account {
     private var id = ""
     private var name = ""
-    private var currency = ""
+    private var currency = "MXN"
     private var balance = Balance()
     private var reports = mutableListOf<Report>()
+
+    constructor(id: String, name: String){
+        this.id = id
+        this.name = name
+        println("Your account $name has been successfully created!!\nNow you can deposit some money here...")
+    }
+
+    constructor(id: String, name: String, currency: String):this(id, name){
+        this.currency = currency
+    }
+
+    constructor(id: String, name: String, currency: String, balance: Balance): this(id,name, currency){
+        this.balance = balance
+    }
+
+    constructor(id: String, name: String, currency: String, balance: Balance, reports: Report): this(id,name,currency, balance){
+        this.reports.add(reports)
+    }
 }
