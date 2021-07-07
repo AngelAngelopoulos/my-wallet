fac563892760453ea02bed7e74e2699725756de9
Jellian <haanniiaarodriguez@gmail.com>
2021-07-07 01:05:46 -0500

    se corrige parte del codigo y se agrega funcion elvis, se verifica excepcion
    Signed-off-by: Jellian <haanniiaarodriguez@gmail.com>

--- src/main/kotlin/personal/Fillable.kt
+++ src/main/kotlin/personal/Fillable.kt
@@ -1,5 +1,7 @@
 package personal
 
+import java.lang.Exception
+
 interface Fillable{
 
     fun fillData(required: String): String{
@@ -11,12 +13,13 @@ interface Fillable{
         return res
     }
 
-    @Suppress("UNCHECKED_CAST")
+    //@Suppress("UNCHECKED_CAST")
     fun editDataList(required: Map<String, String>): List<String>{
 
         var res: String?
 
         val ansList = required.map { (k, v) ->
+            // Saltar con tecla ">"
             println("\nYou can skip this option by entering \">\" ")
             println("$k (Actual value is \"$v\"):")
 
@@ -33,14 +36,16 @@ interface Fillable{
 
             when(res){
                 ">" -> v
-                else -> res
+                // Si sale del while de arriba, no es nulo, pero IDE no lo detecta así, se utiliza...
+                // Elvis ?:p
+                else -> res?: "--"
             }
-
         }
-        return ansList as List<String>
+        return ansList
     }
 
-    fun chooseOption(options: Map<String, String>): String{
+   /* fun chooseOption(options: Map<String, String>): String{
+
         var res= readLine()?.lowercase()
 
         while (!options.containsKey(res)) {
@@ -48,62 +53,84 @@ interface Fillable{
             options.forEach { (k, v) -> println("$k) $v") }
             res = readLine()?.lowercase()
         }
-        return res!!
-    }
+        // Elvis ?:p
+        return res?: "0"
+    }*/
 
     fun fillName(value: String): String{
 
         var res = readLine()
 
-        while (res.isNullOrEmpty() || !isValidName(res)){
+        while (!isValidName(res)){
+            // No es nombre, sino que saltó opción
             if(res== ">")
                 if (value == "--") println("You can't skip this field: \"Name\", because it has not been assigned yet.")
                 else return res
-            else println("Your name can't be empty and can't have special characters either. ")
+            else if (res.isNullOrEmpty()) println("Your name can't be empty")
+            // Caracteres no válidos
+            else println("Your name can't have any special characters. ")
             println("Please enter a valid value.\nName (Actual value is \"$value\"):")
             res = readLine()
         }
-        return res
+        // Si sale del while, no es nulo, pero IDE no lo detecta así, se utiliza...
+        // Elvis ?:p
+        return res?: "--"
     }
 
     fun fillAge(value: String): String{
 
         var res = readLine()
 
-        while (res.isNullOrEmpty() || !isValidAge(res)){
+        while (!isValidAge(res)){
+            // No es edad, sino que saltó opción
             if(res == ">") return res
             else {
                 println("Please enter a valid value.\nAge (Actual value is \"$value\"):")
                 res = readLine()
             }
         }
-        return res
+        // Si sale del while, no es nulo, pero IDE no lo detecta así, se utiliza...
+        // Elvis ?:p
+        return res?.substringBefore('.') ?: "--"
     }
 
-    fun isValidName(name: String): Boolean {
-        // Contiene solo letras y espacios.
-        return (name.filter { it in 'A'..'Z' || it in 'a'..'z' || it == ' '}.length  == name.length)
-                && name.filter { it in 'A'..'Z' || it in 'a'..'z' }.isNotEmpty()
+    fun isValidName(name: String?): Boolean {
+        // Contiene solo letras y espacios & al menos un caracter es una letra.
+        return if (name != null)
+            (name.filter { it in 'A'..'Z' || it in 'a'..'z' || it == ' ' }.length == name.length) && name.any { it in 'A'..'Z' || it in 'a'..'z' }
+        else false
     }
 
-    fun isValidAge(age: String): Boolean{
+    fun isValidAge(age: String?): Boolean{
 
         val iAge: Int
-        // Son todos numeros
-        if (age.filter{ it in '0'..'9'}.length == age.length){
+        //..
+        if (age.isNullOrEmpty()) return false
 
-            iAge = age.toInt()
-
+        // Son todos numeros, positivos, negativos o floats.
+        // Try sustituye al siguiente if:
+        //if (age.filter{ it in '0'..'9' || it == '-' || it == '.'}.length == age.length){
+        try{
+            // Es un numero
+            iAge = age.toFloat().toInt()
             when(iAge) {
-                    in 0..5 -> { println("Babies shouldn't use smartphones..."); return false}
-                    in 6..14 -> { println("Could we have a few words with your parents?"); return false}
-                    in 15..50 -> println("Got it!")
-                    in 51..75 -> println("It's never too late!")
-                    in 76..105 -> println("Wow, we're impressed!")
-                else -> return false
+                in 0..5 -> { println("Babies shouldn't use smartphones..."); return false}
+                in 6..14 -> { println("Could we have a few words with your parents?"); return false}
+                in 15..50 -> println("Got it!")
+                in 51..75 -> println("It's never too late!")
+                in 76..110 -> println("Wow, we're impressed!")
+                else -> { println("Life is that what happens while you're busy trying to fake your age."); return false}
             }
             return true
         }
-        else return false
+        //}
+        // No es número
+        catch(e: Exception) {
+            // Atrapa exception, pero no la muestra
+            //    println("Exception: $e")
+            //else{
+            if(age!=">") println("I just checked the most recent registered numbers updates, and I don't think \"$age\" is a valid one. ")
+            return false
+        }
     }
 }
